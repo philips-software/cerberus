adc2d190e49ac6de5b7cfb5d80a7bf6a18e882cc
Aravind HU <aravind.udayashankara@gmail.com>
2020-10-09 19:23:59 +0530

    Got rid of displayhounds, and render methods and made Cerberus as SpringbootApplication class

--- src/main/java/com/philips/swcoe/cerberus/Cerberus.java
+++ src/main/java/com/philips/swcoe/cerberus/Cerberus.java
@@ -1,18 +1,18 @@
 package com.philips.swcoe.cerberus;
 
+import static com.philips.swcoe.cerberus.constants.DescriptionConstants.CERBERUS_DESCRIPTION;
+import static com.philips.swcoe.cerberus.constants.ProgramConstants.CERBERUS;
+import static com.philips.swcoe.cerberus.constants.ProgramConstants.VERSION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.philips.swcoe.cerberus.hounds.Duplicates;
 import com.philips.swcoe.cerberus.hounds.FindProgrammingMistakes;
 import com.philips.swcoe.cerberus.hounds.JavaCodeMetrics;
 import com.philips.swcoe.cerberus.hounds.JavaCodeMetricsWithDiff;
 import com.philips.swcoe.cerberus.hounds.SuppressedWarnings;
 import picocli.CommandLine;
-import static com.philips.swcoe.cerberus.constants.DescriptionConstants.CERBERUS_DESCRIPTION;
-import static com.philips.swcoe.cerberus.constants.ProgramConstants.CERBERUS;
-import static com.philips.swcoe.cerberus.constants.ProgramConstants.VERSION;
-import static picocli.CommandLine.Model.UsageMessageSpec.SECTION_KEY_COMMAND_LIST;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @CommandLine.Command(
@@ -31,7 +31,6 @@ public class Cerberus {
     public static void main(String[] args) {
         SpringApplication.run(Cerberus.class, args);
         CommandLine cmd = new CommandLine(new Cerberus());
-        cmd.getHelpSectionMap().put(SECTION_KEY_COMMAND_LIST, new DisplayHounds());
         if (args.length < 1) {
             cmd.usage(System.out);
         } else {
